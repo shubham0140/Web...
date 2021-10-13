c5d688e4d25a236ba5944641542370235c17f5c4
shubham0140 <87694344+shubham0140@users.noreply.github.com>
2021-10-13 05:04:09 -0500

    Add files via upload

---
+++ lecture-8/App.js
@@ -0,0 +1,16 @@
+// This is a comment
+
+console.log("Hell World !!");
+
+// String template literals
+let firstName = 'Muskan';
+let lastName = 'Raj';
+
+//let fullName = "Miss." + lastName + "," + firstName;
+//console.log(fullName);
+
+// Better Way-- using String template literals
+let fullName = `Miss.${lastName},${firstName}`;
+
+console.log(fullName);
+
